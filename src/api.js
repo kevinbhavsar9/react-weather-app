49c251dcdcfa1c3e2c11fe706ab93c91617f6eb2
kevin bhavsar <kevinbhavsar567@gmail.com>
2022-08-19 05:45:12 +0530

    add all

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "",// enter your rapid api key here
+    "X-RapidAPI-Key": "e61ac450a4msh10787dea705cb1fp11277ejsn5dd14463b61d", // enter your rapid api key here
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "2b444f7f022038d82d583eafb1cbac86"; // enter your key from openweather API
